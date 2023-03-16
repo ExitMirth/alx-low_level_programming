2a533eaa0853e7fccedb5a30796209c33adb1fa8
ExitMirth <maritimglen@gmail.com>
2023-03-16 13:38:36 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stidoh.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucsess)
+ */
+int main(void)
+{
+	putls("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
